c2d2a904f20263440e2d58bb905d666ae213d947
Edinei J. Bauer <galera.org@gmail.com>
2019-12-05 15:52:52 -0300

    novo modelo

--- public/assets/index.js
+++ public/assets/index.js
@@ -12,7 +12,9 @@ function showEventos() {
                 if(e.ativo) {
                     setTimeout(function () {
                         e.imagem = (!isEmpty(e.imagem) ? e.imagem[0].urls[300] : HOME + "public/assets/img/default.png");
-                        e.data = moment(e.data + " " + e.hora).format("lll");
+                        e.data = moment(e.data_de_inicio).format("ll");
+                        e.home = HOME;
+                        e.vendor = VENDOR;
                         $("#home").append(Mustache.render(tpl.card_read_more, e));
                     }, wait);
                 }
@@ -26,6 +28,38 @@ function showEventos() {
     });
 }
 
+function openCard(id) {
+    $("#evento-content").html("");
+    db.exeRead("eventos", parseInt(id)).then(evento => {
+        $("#evento-content").htmlTemplate("card_post", Object.assign({home: HOME, vendor: VENDOR}, evento)).then(() => {
+            $("#evento").addClass("active");
+        });
+        console.log(evento);
+    });
+}
+
+function closeCard() {
+    $("#evento").removeClass("active animate-top");
+}
+
+var last = 0;
 $(function () {
     showEventos();
+
+    $("#home-parent").on("scroll", function () {
+        let active = Math.round(($(this).scrollLeft() + 350) / 350);
+        if(active !== last) {
+            last = active;
+            $(".card-parent").removeClass("active");
+            $(".card-parent:eq(" + active + ")").addClass("active");
+        }
+    });
+
+    setTimeout(function () {
+        $(".card-parent:eq(1)").addClass("active");
+    }, 300);
+
+    $("#app").off("click", ".card-visitors").on("click", ".card-visitors", function () {
+        openCard($(this).attr("rel"));
+    });
 });
--- public/view/index.php
+++ public/view/index.php
@@ -1,5 +1,14 @@
 <div class="col container-1200">
     <div id="backgroundhome"></div>
     <div id="logohome"></div>
-    <div class="col padding-128" id="home"></div>
+    <div class="col padding-128" id="home-parent">
+        <div class="col padding-24" id="home"></div>
+    </div>
+</div>
+
+<div class="col" id="evento">
+    <div class="btn-close" onclick="closeCard()">
+        <i class="material-icons font-xxlarge font-bold pointer color-primary">close</i>
+    </div>
+    <div class="col padding-24" id="evento-content"></div>
 </div>
